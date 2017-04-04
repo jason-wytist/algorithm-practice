95a5c2abd068677dc48380eda91e5c4ba24fa85a
JoonSoo Ha <json.ha@gmail.com>
2017-04-04 09:29:32 +0900

    sort/shell-sort/js

---
+++ sort/shell-sort/js/shell-sort-basic.js
@@ -0,0 +1,143 @@
+var shellSortBasicASC = function(arr, printHandler) {
+  var n = arr.length;
+  var d, i, j;
+
+  var arrStr = ''; // FOR DEBUG
+  var compCnt = 0; // FOR DEBUG
+  var swapCnt = 0; // FOR DEBUG
+
+  for (d = Math.floor(n/2); d>=1; d = Math.floor(d/2)) {
+    for (i=d; i<n; i++) {
+      for (j=i; j>=d; j=j-d) {
+        showCompMsg(); // FOR DEBUG
+
+        if(!(arr[j] < arr[j-d])) {
+          showArrOnlyComp(); // FOR DEBUG
+          break;
+        }
+
+        if(arr[j] < arr[j-d]) {
+          showSwapMsg(); // FOR DEBUG
+          swapArray(arr, j, j-d);
+          showArrAfterSwap(); // FOR DEBUG
+        }
+      }
+    }
+  }
+
+  showStat(); // FOR DEBUG
+
+
+  /* DEBUGGING FUNCTIONS */
+  function showCompMsg() {
+    if (printHandler !== undefined) {
+      compCnt++;
+      printHandler('compare([' + j + ']=' + arr[j] + ', [' + (j-d) + ']=' + arr[(j-d)] + ')');
+    }
+  }
+
+  function showArrOnlyComp() {
+    if (printHandler !== undefined) {
+      arrStr = generateArrayString(arr, [j, j-d], '(', ')');
+      arrStr += ' // d:' + d + ', i:' + i + ', j:' + j + ', j-d:' + (j-d);
+      printHandler(arrStr);
+      printHandler('');
+    }
+  }
+
+  function showSwapMsg() {
+    if (printHandler !== undefined) {
+      swapCnt++;
+      printHandler('swap([' + j + ']=' + arr[j] + ', [' + (j-d) + ']=' + arr[(j-d)] + ')');
+    }
+  }
+
+  function showArrAfterSwap() {
+    if(printHandler !== undefined) {
+      arrStr = generateArrayString(arr, [j, j-d]);
+      arrStr += ' // d:' + d + ', i:' + i + ', j:' + j + ', j-d:' + (j-d);
+      printHandler(arrStr);
+      printHandler('');
+    }
+  }
+
+  function showStat() {
+    if (printHandler !== undefined) {
+      printHandler('# of compare : ' + compCnt + ', # of swap : ' + swapCnt);
+    }
+  }
+};
+
+
+/////////////////////////////////////////////////////////////////
+
+
+var shellSortBasicDESC = function(arr, printHandler) {
+  var n = arr.length;
+  var d, i, j;
+
+  var arrStr = ''; // FOR DEBUG
+  var compCnt = 0; // FOR DEBUG
+  var swapCnt = 0; // FOR DEBUG
+
+  for (d = Math.floor(n/2); d>=1; d = Math.floor(d/2)) {
+    for (i=d; i<n; i++) {
+      for (j=i; j>=d; j=j-d) {
+        showCompMsg(); // FOR DEBUG
+
+        if(!(arr[j] > arr[j-d])) {
+          showArrOnlyComp(); // FOR DEBUG
+          break;
+        }
+
+        if(arr[j] > arr[j-d]) {
+          showSwapMsg(); // FOR DEBUG
+          swapArray(arr, j, j-d);
+          showArrAfterSwap(); // FOR DEBUG
+        }
+      }
+    }
+  }
+
+  showStat(); // FOR DEBUG
+
+
+  /* DEBUGGING FUNCTIONS */
+  function showCompMsg() {
+    if (printHandler !== undefined) {
+      compCnt++;
+      printHandler('compare([' + j + ']=' + arr[j] + ', [' + (j-d) + ']=' + arr[(j-d)] + ')');
+    }
+  }
+
+  function showArrOnlyComp() {
+    if (printHandler !== undefined) {
+      arrStr = generateArrayString(arr, [j, j-d], '(', ')');
+      arrStr += ' // d:' + d + ', i:' + i + ', j:' + j + ', j-d:' + (j-d);
+      printHandler(arrStr);
+      printHandler('');
+    }
+  }
+
+  function showSwapMsg() {
+    if (printHandler !== undefined) {
+      swapCnt++;
+      printHandler('swap([' + j + ']=' + arr[j] + ', [' + (j-d) + ']=' + arr[(j-d)] + ')');
+    }
+  }
+
+  function showArrAfterSwap() {
+    if(printHandler !== undefined) {
+      arrStr = generateArrayString(arr, [j, j-d]);
+      arrStr += ' // d:' + d + ', i:' + i + ', j:' + j + ', j-d:' + (j-d);
+      printHandler(arrStr);
+      printHandler('');
+    }
+  }
+
+  function showStat() {
+    if (printHandler !== undefined) {
+      printHandler('# of compare : ' + compCnt + ', # of swap : ' + swapCnt);
+    }
+  }
+};
